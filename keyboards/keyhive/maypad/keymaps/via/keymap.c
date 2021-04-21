34ff2988d74b693cda5dec35eec5cfa6e1bb92b6
Ryan Gosiaco <43663778+origamitoaster@users.noreply.github.com>
2021-04-21 04:46:35 -0700

    Update keymap.c

--- keyboards/keyhive/maypad/keymaps/via/keymap.c
+++ keyboards/keyhive/maypad/keymaps/via/keymap.c
@@ -27,30 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P0,   KC_P0,   KC_PDOT, KC_PENT
     )
 };
-
-/*
-#ifdef OLED_DRIVER_ENABLE
-
-static void render_logo(void) {
-    static const char PROGMEM logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(logo, false);
-}
-
-void oled_task_user(void) {
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    render_logo();  // Renders a static logo
-    //oled_scroll_left();  // Turns on scrolling
-}
-
-#endif // OLED_DRIVER_ENABLE
-*/
     
 #ifdef OLED_DRIVER_ENABLE
 
@@ -170,16 +146,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void oled_task_user(void) {
-    //render_anim();
+    render_anim();
     //oled_set_cursor(0,6);
     //sprintf(wpm_str, "       Taps: %03d", tapping);
     //oled_write(wpm_str, false);
-    oled_set_cursor(0,0);
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("Num ") : PSTR("    "), false);
-    oled_set_cursor(0,1);
-    oled_write_P(led_state.caps_lock ? PSTR("Cap ") : PSTR("    "), false);
-    oled_set_cursor(0,0);
-    render_anim();
+    
+    //oled_set_cursor(0,0);
+    //led_t led_state = host_keyboard_led_state();
+    //oled_write_P(led_state.num_lock ? PSTR("Num") : PSTR(""), false);
+    //oled_set_cursor(0,1);
+    //oled_write_P(led_state.caps_lock ? PSTR("Caps") : PSTR(""), false);
 }
 #endif
