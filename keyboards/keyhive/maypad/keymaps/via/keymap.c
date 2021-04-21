623760e8f8cfa50ac861a929a610c4e478c64a03
Ryan Gosiaco <43663778+origamitoaster@users.noreply.github.com>
2021-04-20 20:28:27 -0700

    Update keymap.c

--- keyboards/keyhive/maypad/keymaps/via/keymap.c
+++ keyboards/keyhive/maypad/keymaps/via/keymap.c
@@ -38,7 +38,7 @@ static void render_logo(void) {
     oled_write_P(logo, false);
 }
 
-void oled_task_user(void) {
+void oled_task(void) {
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
